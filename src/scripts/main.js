1b86ae7607d164bccb3647cb0eca61090041f910
José Aquiles <jose.aquiles@ebac.art.br>
2023-04-20 17:05:49 -0300

    primeiro commit

---
+++ src/scripts/main.js
@@ -0,0 +1,15 @@
+//para roda somente depois de todos os arquivos serem carregados
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('form-sorteador').addEventListener('submit',function(evento){
+        evento.preventDefault(evento);
+
+        let numeroMaximo = document.getElementById('numero-maximo').value;
+        numeroMaximo = parseInt(numeroMaximo);
+
+        let numeroAleatorio = Math.random() * numeroMaximo;
+        numeroAleatorio = Math.floor (numeroAleatorio + 1);
+        
+        document.getElementById('resultado-valor').innerText = numeroAleatorio;
+        document.querySelector('.resultado').style.display = 'block';
+    })
+})
